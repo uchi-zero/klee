c8a9f4b4eda0e034d5dd88168e21b32515e22bf1
jiezhuzzz <jiezzz@duck.com>
2025-10-20 11:42:36 -0500

    add more examples

--- examples/brainstorm/09.c
+++ examples/brainstorm/09.c
@@ -1,4 +1,3 @@
-#include "klee/klee.h"
 #include <stdio.h>
 #include <string.h>
 
@@ -14,21 +13,27 @@ int main(int argc, char **argv) {
   }
   char a[40];
   while (fgets(a, sizeof(a), fp) != NULL) {
-    if (memcmp(a, "hello-world", 11) == 0) {
+    if (!memcmp(a, "hello-world", 11)) {
       printf("OK\n");
     }
-    if (memcmp(a, "goodbye-earth", 13) != 0) {
+    if (!memcmp(a, "goodbye-earth", 13)) {
       printf("Also OK\n");
     }
+  }
+  if (!memcmp(a, "space", 5)) {
+    printf("That's the end\n");
   }
   return 0;
 }
 
 // command:
-// ./build/bin/klee --libc=uclibc --posix-runtime --max-time=30s 09.bc A
-// -sym-files 1 40
+// ./build/bin/klee --libc=uclibc --posix-runtime
+// --only-output-states-covering-new --max-time=30s 09.bc A -sym-files 1 40
 // output:
-// KLEE: done: total instructions = 7519530
+// KLEE: done: total instructions = 8315050
 // KLEE: done: completed paths = 0
-// KLEE: done: partially completed paths = 100782
-// KLEE: done: generated tests = 100782
+// KLEE: done: partially completed paths = 112507
+// KLEE: done: generated tests = 4
+// comments:
+// 30s: no "OK" or "Also OK" output
+// 15min: "OK" outputs
