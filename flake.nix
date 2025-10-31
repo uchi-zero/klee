9342452c7b831939dd2048f14957ea675d52924d
jiezhuzzz <jiezzz@duck.com>
2025-10-31 12:22:49 -0500

    Add fenix input and include rust toolchain

--- flake.nix
+++ flake.nix
@@ -7,16 +7,28 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs @ {flake-parts, ...}:
+  outputs = inputs @ {
+    flake-parts,
+    fenix,
+    ...
+  }:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = [
         "x86_64-linux"
         "aarch64-darwin"
       ];
 
-      perSystem = {pkgs, ...}: {
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: {
         formatter = pkgs.alejandra;
 
         devShells.default = pkgs.mkShell.override {stdenv = pkgs.llvmPackages_13.stdenv;} {
@@ -49,7 +61,10 @@
               distutils
               tabulate
               z3-solver
-            ]);
+            ])
+            ++ [
+              (fenix.packages.${system}.stable.toolchain)
+            ];
           env = {
             # Z3_LIBRARY_PATH = "${pkgs.z3.dev}/lib";
             OUT_DIR = "/tmp/klee-out";
