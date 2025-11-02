29adfc4fe13a36e6ce9d74ef107e9423ab205528
jiezhuzzz <jiezzz@duck.com>
2025-11-01 19:05:03 -0500

    Add source code file location analysis and printing plugin pass

--- pass/src/lib.rs
+++ pass/src/lib.rs
@@ -1,38 +1,83 @@
+mod src_loc;
+
 use llvm_plugin::inkwell::values::{FunctionValue, InstructionOpcode};
-use llvm_plugin::{AnalysisKey, FunctionAnalysisManager, LlvmFunctionAnalysis, PassBuilder};
+use llvm_plugin::{
+    AnalysisKey, FunctionAnalysisManager, LlvmFunctionAnalysis, LlvmFunctionPass, PassBuilder,
+    PipelineParsing, PreservedAnalyses,
+};
+use src_loc::SourceLocation;
 
-#[llvm_plugin::plugin(name = "br_counter", version = "0.1")]
-fn plugin_registrar(builder: &mut PassBuilder) {
-    builder.add_function_analysis_registration_callback(|manager| {
-        manager.register_pass(BranchCounterAnalysis);
-    });
-}
+// Analysis pass that collects branch instructions with locations
+struct ForkInstAnalysis;
 
-struct BranchCounterAnalysis;
-
-impl LlvmFunctionAnalysis for BranchCounterAnalysis {
-    type Result = usize;
+impl LlvmFunctionAnalysis for ForkInstAnalysis {
+    type Result = Vec<SourceLocation>;
 
     fn run_analysis(
         &self,
         function: &FunctionValue,
         _manager: &FunctionAnalysisManager,
     ) -> Self::Result {
-        let mut count = 0;
+        let mut locations = Vec::new();
 
-        for basic_block in function.get_basic_blocks() {
-            for instruction in basic_block.get_instructions() {
-                if instruction.get_opcode() == InstructionOpcode::Br {
-                    count += 1;
+        for bb in function.get_basic_blocks() {
+            for inst in bb.get_instructions() {
+                if inst.get_opcode() == InstructionOpcode::Br {
+                    if let Some(info) = SourceLocation::from_instruction(&inst) {
+                        locations.push(info);
+                    }
                 }
             }
         }
 
-        count
+        locations
     }
 
     fn id() -> AnalysisKey {
         static ID: u8 = 0;
         &ID
     }
+}
+
+// Printer pass that queries the analysis and prints results
+struct BranchPrinterPass;
+
+impl LlvmFunctionPass for BranchPrinterPass {
+    fn run_pass(
+        &self,
+        function: &mut FunctionValue,
+        manager: &FunctionAnalysisManager,
+    ) -> PreservedAnalyses {
+        let locations = manager.get_result::<ForkInstAnalysis>(function);
+        let function_name = function.get_name().to_str().unwrap();
+
+        println!(
+            "Function '{}' has {} branch instruction(s):",
+            function_name,
+            locations.len()
+        );
+
+        for loc in locations.iter() {
+            println!("{:?}", loc);
+        }
+
+        PreservedAnalyses::All
+    }
+}
+
+// Plugin registration
+#[llvm_plugin::plugin(name = "branch_counter", version = "0.1")]
+fn plugin_registrar(builder: &mut PassBuilder) {
+    builder.add_function_analysis_registration_callback(|manager| {
+        manager.register_pass(ForkInstAnalysis);
+    });
+
+    builder.add_function_pipeline_parsing_callback(|name, pass_manager| {
+        if name == "branch-printer" {
+            pass_manager.add_pass(BranchPrinterPass);
+            PipelineParsing::Parsed
+        } else {
+            PipelineParsing::NotParsed
+        }
+    });
 }
---
+++ pass/src/src_loc.rs
@@ -0,0 +1,62 @@
+use llvm_plugin::inkwell::llvm_sys::debuginfo::{
+    LLVMDIFileGetDirectory, LLVMDIFileGetFilename, LLVMDILocationGetColumn, LLVMDILocationGetLine,
+    LLVMDILocationGetScope, LLVMDIScopeGetFile, LLVMInstructionGetDebugLoc,
+};
+use llvm_plugin::inkwell::values::{AsValueRef, InstructionValue};
+use std::path::PathBuf;
+
+/// Store branch info with location
+#[derive(Debug)]
+pub struct SourceLocation {
+    pub filepath: PathBuf,
+    pub line: u32,
+    pub column: u32,
+}
+
+fn non_null<T>(ptr: *mut T) -> Option<*mut T> {
+    if ptr.is_null() { None } else { Some(ptr) }
+}
+
+impl SourceLocation {
+    /// Extract branch information from an LLVM instruction
+    pub fn from_instruction(inst: &InstructionValue) -> Option<Self> {
+        unsafe {
+            let debug_loc = non_null(LLVMInstructionGetDebugLoc(inst.as_value_ref()))?;
+            let scope = non_null(LLVMDILocationGetScope(debug_loc))?;
+            let file = non_null(LLVMDIScopeGetFile(scope))?;
+
+            let mut filename_len: u32 = 0;
+            let mut directory_len: u32 = 0;
+            let filename_ptr = LLVMDIFileGetFilename(file, &mut filename_len);
+            let directory_ptr = LLVMDIFileGetDirectory(file, &mut directory_len);
+
+            let filename_str = if !filename_ptr.is_null() && filename_len > 0 {
+                std::slice::from_raw_parts(filename_ptr as *const u8, filename_len as usize)
+            } else {
+                &[]
+            };
+
+            let directory_str = if !directory_ptr.is_null() && directory_len > 0 {
+                std::slice::from_raw_parts(directory_ptr as *const u8, directory_len as usize)
+            } else {
+                &[]
+            };
+            if filename_str.is_empty() && directory_str.is_empty() {
+                return None;
+            }
+            let path = if !directory_str.is_empty() {
+                let dir = String::from_utf8_lossy(directory_str);
+                let file = String::from_utf8_lossy(filename_str);
+                PathBuf::from(dir.as_ref()).join(file.as_ref())
+            } else {
+                PathBuf::from(String::from_utf8_lossy(filename_str).as_ref())
+            };
+
+            return Some(SourceLocation {
+                filepath: path,
+                line: LLVMDILocationGetLine(debug_loc),
+                column: LLVMDILocationGetColumn(debug_loc),
+            });
+        }
+    }
+}
